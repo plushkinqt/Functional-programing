f00cc1ce0454b67d91a769d011920077bac1b7df
plushkinqt <plushkinqt@gmail.com>
2015-10-27 13:01:20 +0200

    pattern matching with tail recursion

--- src/TailRecursion/Factorial.scala
+++ src/TailRecursion/Factorial.scala
@@ -9,8 +9,23 @@ object Factorial{
    * because it uses only one frame and if compiler optimized then it's faster then forward recursion
    */
   def calculate (x: Int) :Int = {
-    def loop(remainder: Int, current: Int): Int = {
+    /**
+     * Helper function for tail recursive loop
+     */
+    def loop(remainder: Int, current: Int): Int =
       if (remainder == 0) current else loop(remainder-1, current * remainder)
+    loop(x,1)
+  }
+
+  /**
+   * Same function with pattern matching
+   */
+  def factorial (x: Int) :Int = {
+    def loop(remainder: Int, current: Int): Int = {
+      remainder match {
+        case 0 => current
+        case _ => loop(remainder-1, current * remainder)
+      }
     }
     loop(x,1)
   }
