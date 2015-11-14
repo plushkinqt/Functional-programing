dfd8224b1a2c7e976f247badfd617c9bab813441
plushkinqt <plushkinqt@gmail.com>
2015-11-14 14:42:38 +0200

    answer added

--- src/ProjectEuler/Problem9.scala
+++ src/ProjectEuler/Problem9.scala
@@ -4,15 +4,15 @@ package ProjectEuler
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
  */
-object Problem9 {
+object Problem9 extends App{
 
   def calculate(sum: Int): Int ={
-    for(c <- 1 to 998; a<- 1 until c){
+    for(c <- 1 to sum -2; a<- 1 until c){
       val b = sum - a - c
       if(a*a+b*b == c*c) return a*b*c
     }
     -1
   }
   val result = calculate(1000)
-
+  //Answer: 31875000
 }
